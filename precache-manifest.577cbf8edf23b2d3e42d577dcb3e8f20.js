6d01ff870224e29329d6583e4b335bf41161769c
ruinername <russhianhere@gmail.com>
2019-04-09 16:54:33 +0300

    1

---
+++ precache-manifest.577cbf8edf23b2d3e42d577dcb3e8f20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3bc4cbcacf1e44c8dbae",
+    "url": "/static/js/main.3bc4cbca.chunk.js"
+  },
+  {
+    "revision": "f3f15c0d0e6570dc7212",
+    "url": "/static/js/2.f3f15c0d.chunk.js"
+  },
+  {
+    "revision": "3bc4cbcacf1e44c8dbae",
+    "url": "/static/css/main.d3fdcb97.chunk.css"
+  },
+  {
+    "revision": "f3f15c0d0e6570dc7212",
+    "url": "/static/css/2.4f3eb583.chunk.css"
+  },
+  {
+    "revision": "d1fbeac2df9b5b5feda22e11542bb0d0",
+    "url": "/index.html"
+  }
+];
